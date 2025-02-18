93535f2eff987a094afb5313cce333f3ec413099
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-02-18 22:55:26 +0900

    feat: 종료 입력 시 프로그램 종료

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 package com.ll
 
-import java.io.IO.println
 
 fun main() {
-    println("Hello World!")
+    println("== 명언 앱 ==")
+
+    while (true) {
+        print("명령) ")
+        val cmd = readlnOrNull()!!.trim()    //cmd에 null 일 리가 없기 때문에 !! 삽입    null일 수 있다면 ? 사용
+        if (cmd == "종료") {
+            break
+        }
+
+    }
 }
