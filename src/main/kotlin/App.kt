74be9a0a1f62002138a9a3ad5941a6d9e0de9c7a
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-02-19 12:47:46 +0900

    refactor: Rq클래스 도입

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,27 +1,31 @@
 package com.ll
 
+import com.ll.com.ll.Rq
 import com.ll.com.ll.WiseSaying
 
 class App {
         fun run() {
             println("== 명언 앱 ==")
             val wiseSayingList = mutableListOf<WiseSaying>()
-            var lastId = 1;
+            var lastId = 0;
             while (true) {
                 print("명령) ")
+
                 val cmd = readlnOrNull()!!.trim()    //cmd에 null 일 리가 없기 때문에 !! 삽입    null일 수 있다면 ? 사용
-                if (cmd == "종료") {
+
+                val rq = Rq(cmd)
+
+                if (rq.action == "종료") {
                     break
-                } else if (cmd == "등록") {
+                } else if (rq.action == "등록") {
                     print("명언 : ")
                     val wise = readlnOrNull()!!.trim()
                     print("작가 : ")
                     val author = readlnOrNull()!!.trim()
-                    val wiseSaying = WiseSaying(lastId, wise, author)
+                    val wiseSaying = WiseSaying(++lastId, wise, author)
                     println("${lastId}번 명언이 등록되었습니다.")
                     wiseSayingList.add(wiseSaying)
-                    lastId++
-                } else if (cmd == "목록") {
+                } else if (rq.action == "목록") {
                     if (wiseSayingList.isEmpty()) {
                         println("등록된 명언이 없습니다.")
                         continue
@@ -31,12 +35,18 @@ class App {
                     for (wiseSaying in wiseSayingList.sortedByDescending { it.id }) {
                         println("${wiseSaying.id} / ${wiseSaying.author} / ${wiseSaying.wise}")
                     }
-                } else if (cmd == "삭제") {
-                    print("삭제?id=")
-                    val deleteId: Int = readln().toInt()
+                } else if (rq.action == "삭제") {
+                    val id = rq.getParamValueAsInt("id", 0)
+                    if (id == 0) {
+                        println("id를 입력해주세요.")
+                        continue
+                    }
 
-                    wiseSayingList.removeIf{it.id == deleteId}
-                    println("${deleteId}번 명언이 삭제되었습니다.")
+                    println("입력된 id : $id")
+
+                    wiseSayingList.removeIf{ it.id == id }
+
+                    println("${id}번 명언을 삭제하였습니다.")
                 }
             }
         }
