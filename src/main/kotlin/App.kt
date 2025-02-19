abccd61286cf8d3533bd3eb792e8eea725c79a76
성기범 <133200703+sungkibum@users.noreply.github.com>
2025-02-19 12:01:45 +0900

    feat: 삭제 기능 추가

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -6,7 +6,7 @@ class App {
         fun run() {
             println("== 명언 앱 ==")
             val wiseSayingList = mutableListOf<WiseSaying>()
-            var num = 1;
+            var lastId = 1;
             while (true) {
                 print("명령) ")
                 val cmd = readlnOrNull()!!.trim()    //cmd에 null 일 리가 없기 때문에 !! 삽입    null일 수 있다면 ? 사용
@@ -17,10 +17,10 @@ class App {
                     val wise = readlnOrNull()!!.trim()
                     print("작가 : ")
                     val author = readlnOrNull()!!.trim()
-                    val wiseSaying = WiseSaying(num, wise, author)
-                    println("${num}번 명언이 등록되었습니다.")
+                    val wiseSaying = WiseSaying(lastId, wise, author)
+                    println("${lastId}번 명언이 등록되었습니다.")
                     wiseSayingList.add(wiseSaying)
-                    num++
+                    lastId++
                 } else if (cmd == "목록") {
                     if (wiseSayingList.isEmpty()) {
                         println("등록된 명언이 없습니다.")
@@ -31,9 +31,15 @@ class App {
                     for (wiseSaying in wiseSayingList.sortedByDescending { it.id }) {
                         println("${wiseSaying.id} / ${wiseSaying.author} / ${wiseSaying.wise}")
                     }
-                    }
+                } else if (cmd == "삭제") {
+                    print("삭제?id=")
+                    val deleteId: Int = readln().toInt()
+
+                    wiseSayingList.removeIf{it.id == deleteId}
+                    println("${deleteId}번 명언이 삭제되었습니다.")
                 }
             }
         }
+}
 
 
